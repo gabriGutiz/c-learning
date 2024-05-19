e89574c1d53fa8f8450a5278865a1b706b1e56eb
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-05-18 23:10:09 -0300

    fix (libft): change name of ft_calloc and ft_strdup

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -2,6 +2,7 @@
 #define LIBFT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct       s_list
 {
@@ -31,8 +32,8 @@ void                 *ft_memchr(const void *str, int c, size_t n);
 int                  ft_memcmp(const void *str1, const void *str2, size_t n);
 char                 *ft_strnstr(const char *s, const char *find, size_t slen);
 int                  ft_atoi(const char *str);
-void                 *calloc(size_t nitems, size_t size);
-char                 *strdup(const char *str);
+void                 *ft_calloc(size_t nitems, size_t size);
+char                 *ft_strdup(const char *str);
 
 // Additional
 char                 *ft_substr(char const *s, unsigned int start, size_t len);
